9ae7f27dbfbaf3a86290e9d33d4ecfe4f3eb09c8
ralphdev <rafa.developers@gmail.com>
2024-06-14 11:25:01 -0500

    Calculando total a pagar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,32 +2,34 @@ export default function Header({ cart }) {
 
   //State Derivado
   const isEmpty = () => cart.length === 0;
-  return (
-    <>
-      <header className="py-5 header">
-        <div className="container-xl">
-          <div className="row justify-content-center justify-content-md-between">
-            <div className="col-8 col-md-3">
-              <a href="index.html">
-                <img
-                  className="img-fluid"
-                  src="./public/img/logo.svg"
-                  alt="imagen logo"
-                />
-              </a>
-            </div>
-            <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-              <div className="carrito">
-                <img
-                  className="img-fluid"
-                  src="./public/img/carrito.png"
-                  alt="imagen carrito"
-                />
+  const cartTotal = () => cart.reduce((total, item) => total + (item.quantity * item.price), 0);
 
-                <div id="carrito" className="bg-white p-3">
-                  {isEmpty() ? (
-                    <p className="text-center">El carrito esta vacio</p>
-                  ) : (
+  return (
+    <header className="py-5 header">
+      <div className="container-xl">
+        <div className="row justify-content-center justify-content-md-between">
+          <div className="col-8 col-md-3">
+            <a href="index.html">
+              <img
+                className="img-fluid"
+                src="./public/img/logo.svg"
+                alt="imagen logo"
+              />
+            </a>
+          </div>
+          <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+            <div className="carrito">
+              <img
+                className="img-fluid"
+                src="./public/img/carrito.png"
+                alt="imagen carrito"
+              />
+
+              <div id="carrito" className="bg-white p-3">
+                {isEmpty() ? (
+                  <p className="text-center">El carrito esta vacio</p>
+                ) : (
+                  <>
                     <table className="w-100 table">
                       <thead>
                         <tr>
@@ -68,20 +70,20 @@ export default function Header({ cart }) {
                         ))}
                       </tbody>
                     </table>
-                  )}
 
-                  <p className="text-end">
-                    Total pagar: <span className="fw-bold">$899</span>
-                  </p>
-                  <button className="btn btn-dark w-100 mt-3 p-2">
-                    Vaciar Carrito
-                  </button>
-                </div>
+                    <p className="text-end">
+                      Total pagar: <span className="fw-bold">${cartTotal()}</span>
+                    </p>
+                  </>
+                )}
+                <button className="btn btn-dark w-100 mt-3 p-2">
+                  Vaciar Carrito
+                </button>
               </div>
-            </nav>
-          </div>
+            </div>
+          </nav>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 }
