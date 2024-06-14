e0446bb79f0b9bb33676a3b73b54c5c5ca318c5b
ralphdev <rafa.developers@gmail.com>
2024-06-13 19:37:17 -0500

    Hooks, useEffect, db Product State, Iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
+import { db } from "./data/db"
 
 export default function App() {
+
+    const [data, setData] = useState(db);
+
+    console.log(data);
 
     return (
         <>
 
-    <Header/>
+        <Header/>
 
-    <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección</h2>
+        <main className="container-xl mt-5">
+            <h2 className="text-center">Nuestra Colección</h2>
 
-        <div className="row mt-5">
+            <div className="row mt-5">
+                {data.map( () => (
 
-            <Guitar/>
-        </div>
-    </main>
+                    <Guitar/>
+                ))}
+            </div>
+        </main>
 
 
-    <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-        </div>
-    </footer>
-    </>
+        <footer className="bg-dark mt-5 py-5">
+            <div className="container-xl">
+                <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+            </div>
+        </footer>
+        </>
   )
 }
