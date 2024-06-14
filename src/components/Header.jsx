e1881820c9ae62c4de0d5a3867a3f43fe34d0971
ralphdev <rafa.developers@gmail.com>
2024-06-14 11:12:40 -0500

    State derivado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 export default function Header({ cart }) {
+
+  //State Derivado
+  const isEmpty = () => cart.length === 0;
   return (
     <>
       <header className="py-5 header">
@@ -22,7 +25,7 @@ export default function Header({ cart }) {
                 />
 
                 <div id="carrito" className="bg-white p-3">
-                  {cart.length === 0 ? (
+                  {isEmpty() ? (
                     <p className="text-center">El carrito esta vacio</p>
                   ) : (
                     <table className="w-100 table">
